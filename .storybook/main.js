23b26237ec42d512742e8c798d9b7b2a33dec76d
ivanunaffi <ivan@unaffi.com>
2025-08-13 15:05:51 +0200

    Refactoring packaging

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,8 @@ const config = {
     "../src/**/*.stories.@(js|ts|svelte)"
   ],
   "addons": [
-    "@storybook/addon-svelte-csf"
+    "@storybook/addon-svelte-csf",
+    "@storybook/addon-docs"
   ],
   "framework": {
     "name": "@storybook/sveltekit",
